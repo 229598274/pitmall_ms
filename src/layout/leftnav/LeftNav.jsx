4a7498a29287589c4a174765a823b19ff2167c50
rbyu <rongbo.yu@myxxjs.com>
2019-09-30 15:38:22 +0800

    ok

--- src/layout/leftnav/LeftNav.jsx
+++ src/layout/leftnav/LeftNav.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
-import UserManagement from '../../views/user/User';
-import AuthorityManagement from '../../views/authority/Authority';
-import DepartmentManagement from '../../views/department/Department';
-import RoleManagement from '../../views/role/Role';
+import User from '../../views/user/User';
+import Authority from '../../views/authority/Authority';
+import Department from '../../views/department/Department';
+import Role from '../../views/role/Role';
 import Home from '../../views/home/Home';
+import Authorize from '../../views/authorize/Authorize';
 
 import './leftnav.css';
 
@@ -39,7 +40,7 @@ class LeftNav extends React.Component {
                   </Link>
                 </Menu.Item>
                 <Menu.Item key="1">
-                  <Link to="/userManagement" >
+                  <Link to="/user" >
                     <Icon type="user" />
                     <span className="nav-text">用户管理</span>
                   </Link>
@@ -54,18 +55,23 @@ class LeftNav extends React.Component {
                   }
                 >
                   <Menu.Item key="2">
-                    <Link to="/departmentManagement">
+                    <Link to="/department">
                       <span className="nav-text">部门管理</span>
                     </Link>
                   </Menu.Item>
                   <Menu.Item key="3">
-                    <Link to="/roleManagement">
+                    <Link to="/role">
                       <span className="nav-text">角色管理</span>
                     </Link>
                   </Menu.Item>
                   <Menu.Item key="4">
-                    <Link to="/authorityManagement">
+                    <Link to="/authority">
                       <span className="nav-text">权限管理</span>
+                    </Link>
+                  </Menu.Item>
+                  <Menu.Item key="5">
+                    <Link to="/authorize">
+                      <span className="nav-text">授权管理</span>
                     </Link>
                   </Menu.Item>
                 </SubMenu>
@@ -74,10 +80,11 @@ class LeftNav extends React.Component {
             <Layout>
               <Header style={{ background: '#fff', padding: 0 }} />
               <Route path="/" exact component={Home} />
-              <Route path="/userManagement" component={UserManagement} />
-              <Route path="/roleManagement" component={RoleManagement} />
-              <Route path="/departmentManagement" component={DepartmentManagement} />
-              <Route path="/authorityManagement" component={AuthorityManagement} />
+              <Route path="/user" component={User} />
+              <Route path="/role" component={Role} />
+              <Route path="/department" component={Department} />
+              <Route path="/authority" component={Authority} />
+              <Route path="/authorize" component={Authorize} />
               <Footer style={{ textAlign: 'center' }}>PitMall 2019 Created</Footer>
             </Layout>
           </Layout>
--- src/views/authorize/Authorize.jsx
+++ src/views/authorize/Authorize.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb, Icon, Table, Divider, Button,Tag,Row,Col,Pagination } from 'antd';
-
+import { Layout, Menu, Breadcrumb, Icon, Table, Divider, Button, Tag, Row, Col, Pagination } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
 class Authorize extends React.Component {
     render() {
         return (
-
-                <Content style={{ margin: '0 16px' }}>
-                    <Breadcrumb style={{ margin: '16px 0' }}>
-                        <Breadcrumb.Item>权限管理</Breadcrumb.Item>
-                        <Breadcrumb.Item>授权管理</Breadcrumb.Item>
-                    </Breadcrumb>
-                    <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-                        授权管理
+            <Content style={{ margin: '0 16px' }}>
+                <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb.Item>权限管理</Breadcrumb.Item>
+                    <Breadcrumb.Item>授权管理</Breadcrumb.Item>
+                </Breadcrumb>
+                <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+                    授权管理
                     </div>
-                </Content>
- 
+            </Content>
         )
     }
 }
